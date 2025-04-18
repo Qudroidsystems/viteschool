7879f8e037bef066e108bebc5a66258c2b252517
Eliab <eliabsiji@gmail.com>
2025-04-18 09:10:44 +0100

    app title changed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" >
   <!--begin::Head-->
   <head>
-    <title>Scroll | Dashboard</title>
+    <title>ViteeSchool</title>
     <meta charset="utf-8"/>
     <meta name="description" content=" "/>
     <meta name="keywords" content=""/>
